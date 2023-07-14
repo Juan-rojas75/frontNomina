413041217a805f890ac10a75f827dfba081b48ba
Desarrollo3HQ5 <desarrollo3@hq5.com.co>
2023-07-13 21:43:40 -0500

    .

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { ContactComponent } from './components/contact/contact.component';
 import { HomeComponentComponent } from './components/home-component/home-component.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 const routes: Routes = [
   { path: "", component: HomeComponentComponent },
   { path: "contactus", component: ContactComponent },
+  { path: "login", component: LoginComponent },
 ];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
